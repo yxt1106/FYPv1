d3944a55eedd365f8803124396584fc83d98bc3a
yxt1106 <142809358+yxt1106@users.noreply.github.com>
2025-03-05 18:19:00 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// import { createApp } from 'vue/dist/vue.esm-bundler';
-import { createApp } from 'vue/dist/vue.runtime.esm-bundler';
+import { createApp } from 'vue/dist/vue.esm-bundler';
+// import { createApp } from 'vue/dist/vue.runtime.esm-bundler';
 import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
